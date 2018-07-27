fe833a6d1c909efaa805a8f089524af5f9233269
Flynn <812264301@qq.com>
2018-07-27 23:00:41 +0800

    unfinished project

---
+++ RuleBaseBlock/RuleBaseBlock/RuleBaseBlockComponent.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace RuleBaseBlock
+{
+    public class RuleBaseBlockComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public RuleBaseBlockComponent()
+          : base("RuleBaseBlock", "Rule",
+              "Rule base module",
+              "RuleBase", "RuleBaseDemo")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Graphic Infomation Path", "GIP", "Graphic Infomation Path", GH_ParamAccess.item);
+            pManager.AddTextParameter("Attribute Information Path", "AIP", "Attribute Infomation Path", GH_ParamAccess.item);
+            pManager.AddTextParameter("RuleBase", "RB", "Rule Base", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Result Analysis Report", "RAP", "Result Analysis Report", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string GraPath = "";
+            string AttPath = "";
+            string RuleBase = "";
+            if (!DA.GetData(0, ref GraPath)) { return; }
+            if (!DA.GetData(1, ref AttPath)) { return; }
+            if (!DA.GetData(2, ref RuleBase)) { return; }
+            //正则表达式解析RuleBase中functionname
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("3d8072a8-2a90-4be4-b6e0-868cb9cb9d88"); }
+        }
+    }
+}
---
+++ RuleBaseBlock/RuleBaseBlock/RuleBaseBlockInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace RuleBaseBlock
+{
+    public class RuleBaseBlockInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "RuleBaseBlock";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("b2757a56-a388-4fde-8454-b88868af6c1e");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
